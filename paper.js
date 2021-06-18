7672170e6074acb79f212ae6eb2b4888f1aae01b
MMNJ1709 <84569397+MMNJ1709@users.noreply.github.com>
2021-06-18 15:59:58 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,30 @@
+class paper{
+	constructor(x,y,r)
+	{
+	// assign options to the rubber ball
+	var options={
+		restitution:0.3,
+		friction:5,
+		density:1
+	}
+		this.x=x;
+		this.y=y;
+		this.r=r
+        this.image=loadImage("paper.png")
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			imageMode(CENTER)
+
+			//draw the ellipse here to display the rubber ball
+image(this.image,0,0,this.r,this.r)
+			pop()
+	}
+
+}
